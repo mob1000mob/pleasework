316668218ac35b94395711033c6d1540eaf709f3
mob1000mob <160032318+mob1000mob@users.noreply.github.com>
2024-06-18 14:55:53 +0800

    Update db.php

--- include/db.php
+++ include/db.php
@@ -1,5 +1,5 @@
 <?php
-	define('DB_HOST', 'phub.mysql.database.azure.com');
+	define('DB_HOST', 'group6server.mysql.database.azure.com');
 	define('DB_USER', 'phubgroup6');
 	define('DB_PASS', 'MamAbiFinals6');
 	define('DB_NAME', 'portfoliohub');
